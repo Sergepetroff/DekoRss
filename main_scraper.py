e837e5c578db93ff72cf23e87074a363c79a1969
Sergepetroff <Sergepetroff@users.noreply.github.com>
2025-10-05 15:10:53 +0000

    deploy: 3948d71c54eadc4302a5904aacc563b8b8169687

--- main_scraper.py
+++ main_scraper.py
@@ -42,13 +42,12 @@ async def login_and_scrape(page):
     except Exception as e:
         print(f"Кнопка 18+ отсутствует или ошибка: {e}")
 
-    await page.wait_for_selector("div.entry-wrap--post", timeout=30000)
+    await page.wait_for_selector("div.entry-wrap--post", timeout=180000)
 
 async def scrape_and_generate_rss():
     async with async_playwright() as p:
         print("Запуск браузера...")
-        browser = await p.chromium.launch(headless=True, args=["--no-sandbox"])
-
+        browser = await p.chromium.launch(headless=True) #, args=["--no-sandbox"])
         page = await browser.new_page(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
 
         try:
