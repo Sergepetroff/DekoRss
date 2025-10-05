ede1e252cc8c1d07d9b6f91496ca9d0cc73707a9
Sergepetroff <Sergepetroff@gmail.com>
2025-10-05 12:31:10 +0200

    Update main_scraper.py
    run headless

--- main_scraper.py
+++ main_scraper.py
@@ -41,7 +41,7 @@ async def login_and_scrape(page):
 async def scrape_and_generate_rss():
     async with async_playwright() as p:
         print("Запуск браузера...")
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
         page.on("requestfailed", lambda request: print(f"Request failed: {request.url}"))
 
